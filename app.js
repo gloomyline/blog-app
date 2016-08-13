80d3f1d549b61bd52c9065eb627665e577e928da
unknown <1211071880@qq.com>
2016-08-13 15:00:52 +0800

    mongoose无法契合koa暂时未找到解决办法，决定使用monk

--- app.js
+++ app.js
@@ -13,10 +13,65 @@ var render = views(__dirname + '/views', {
 })
 
 // 连接数据库
-var db = require('./db')
+var Blog = require('./db')
 
 // 路由中间件
 // app.use(route.get('/', list))
+app.use(route.post('/post', create))
+app.use(route.get('/post/new', add))
+app.use(route.get('/post/:id', show))
+
+// function* list() {
+//     var that = this
+//     Blog.find({}).exec((err, data) => {
+//         if (err) {
+//             console.log(err)
+//         } else {
+//             data = data.toObject()
+//             data.id = data._id
+//             delete data._id
+//             console.log(data)
+//             if (!data) that.throw(404, 'invalid data');
+//             // that.body = yield render('list', { data: data })
+//         }
+//     })
+// }
+
+function* add() {
+    this.body = yield render('new')
+}
+
+function* show(id) {
+    // console.log(id)
+    var _data = {}
+    Blog.findById(id, (err, data) => {
+        if (err) {
+            console.log(err)
+        } else {
+            data = data.toObject();
+            data.id = data._id;
+            delete data._id;
+            // console.log(data)
+            if (!data) this.throw(404, 'invalid data id');
+            // console.log(this)
+            // this.body = render('show', { data: data })
+            _data = data
+        }
+    });
+    console.log(_data)
+    this.body = yield render('show', { data: _data })
+}
+
+function* create() {
+    var post = yield parse(this)
+    post.create_time = new Date()
+    var blog = new Blog(post)
+    blog.save((err) => {
+        if (err) {
+            console.log(err)
+        }
+    })
+}
 
 app.listen(3333);
 console.log('listening on port 3333')
